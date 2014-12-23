7be9e7bfe471f1e5ee1e1aa4e1f9ce9e49b9cfe5
jlfy <julienlfy@gmail.com>
2014-12-23 23:08:12 +0100

    Choice between ascii view and html5 canvas view with some options

--- src/views/jq-player.js
+++ src/views/jq-player.js
@@ -4,8 +4,10 @@
 
         // Default options
         var defaults = {
-            position:'',
-            notation:''
+            type:'canvas',
+            cvSquareSize:40,
+            cvSquareDarkColor:'#B4814E',
+            cvSquareLightColor:'#FFFFFF'
         };
 
         // avoid $(this) confusion
@@ -19,21 +21,16 @@
         // HTML item reference
         var element = element;
 
-
-
         plugin.init = function() {
             plugin.options = $.extend({}, defaults, options);
             
-            var param1 = plugin.options['param1'];
             var id = $element.attr("id");
             var position = $element.data('position');
             var notation = $element.data('notation');
-
-            var game = DamWeb.getGame(position, notation);
+            game = DamWeb.getGame(position, notation);
             game.start(); 
             plugin.refresh(game);
 
-            
 
             $("body").on("click","span",function(){
                 var pos = $(this).data('pos');
@@ -60,20 +57,21 @@
                 game.end();
                 plugin.refresh(game);
             });
-        }
-
+        };
 
         plugin.refresh = function(game){
             var notation = plugin.drawNotation(game.getNotation());
-            var board = plugin.drawBoard(game.board);
             var ctrlBar = plugin.drawControlBar();
-
 
             var layout = '';
             layout += '<table>';
             layout += '<tr>';
             layout += '<td>';
-            layout += board;
+            if (plugin.options['type'] == "canvas"){
+                layout += plugin.getCanvas();
+            } else if (plugin.options['type'] == "ascii"){
+                layout += plugin.drawBoard(game.board);
+            }
             layout += '</td>';
             layout += '<td>';
             layout += notation;
@@ -87,9 +85,15 @@
             layout += '</tr>';
             layout += '</table>';
 
-            $(element).html(layout);
-        }
 
+            $(element).html(layout);
+
+            if (plugin.options['type'] == "canvas"){
+                var fgColor = plugin.options['cvSquareDarkColor'];
+                var bgColor = plugin.options['cvSquareLightColor'];
+                drawCanvasContent("canvas", game.board, fgColor, bgColor);
+            }
+        };
 
         plugin.drawNotation = function(notation){
 
@@ -171,8 +175,7 @@
             ht += '</div>';
 
             return ht;
-        }
-
+        };
 
         plugin.drawControlBar = function(){
             var ht = '';
@@ -185,7 +188,7 @@
             ht += '</div>';
 
             return ht;
-        }
+        };
 
         plugin.drawBoard = function(board){
             var ht = '';
@@ -195,91 +198,213 @@
             ht += '</div>';
             
             return ht;
-        }
+        };
+
+        plugin.getCanvas = function(board){
+            var ht = '';
+            ht += '<canvas id="canvas">';
+            ht += '</canvas>';
+            return ht;
+        };
+
+
+
+        var getNotationStyle = function(notation){
+            var idxSep = notation.indexOf("-");
+            if (idxSep == -1) {
+                idxSep = notation.indexOf("x");
+                notation = notation.replace("x", "&times;");
+            } else {
+                notation = notation.replace("-", "&minus;");
+            }
+            var s = notation.substring(idxSep + 1);
+
+            var before = (idxSep == 1);
+            var after = (s.length == 1);
+
+            var map = { 'notation':notation, 'before':before, 'after':after };
+            return map;
+        };
+
+        var getASCIIBoard = function(board) {
+            var s = "";
+            
+            var cr = "<br />";
+            var sp = "&nbsp;";
+            var row = "";
+
+            s += "================================" + cr;
+            for (var k = 0; k < 10; k++) {
+                row += "|";
+                for (var l = 1; l <= 5; l++) {
+
+                    var num = 5 * k + l;
+                    var p = board.getSquare(num).piece;
+
+                    if (k == 0 || k == 2 || k == 4 || k == 6 || k == 8) {
+                        row += sp + sp + sp;
+                    }
+
+                    switch (p) {
+                    case Piece.PAWN_WHITE:
+                        row += sp + "o" + sp;
+                        break;
+                    case Piece.PAWN_BLACK:
+                        row += sp + "x" + sp;
+                        break;
+                    case Piece.DAME_WHITE:
+                        row += sp + "O" + sp;
+                        break;
+                    case Piece.DAME_BLACK:
+                        row += sp + "X" + sp;
+                        break;
+                    default:
+                        row += sp + "." + sp;
+                    }
+
+                    if (k == 1 || k == 3 || k == 5 || k == 7 || k == 9) {
+                        row += sp + sp + sp;
+                    }
+                }
+                row += "|";
+
+                s += row + cr;
+                row = "";
+            }
+            s += "================================";
+
+            return s;
+        };
+
+        var drawPiece = function(ctx, piece, x, y, r){
+            var sqWidth = plugin.options['cvSquareSize'];
+            var shift = ((~~(sqWidth / 10)) + 1) / 2;
+
+            switch (piece) {
+            case Piece.PAWN_WHITE:
+                ctx.beginPath();
+                ctx.arc(x, y, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "black";
+                ctx.fillStyle = "white";
+                ctx.fill();
+                ctx.stroke();
+                break;
+            case Piece.PAWN_BLACK:
+                ctx.beginPath();
+                ctx.arc(x, y, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "white";
+                ctx.fillStyle = "black";
+                ctx.fill();
+                ctx.stroke();
+                break;
+            case Piece.DAME_WHITE:
+                ctx.beginPath();
+                ctx.arc(x, y + shift, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "black";
+                ctx.fillStyle = "white";
+                ctx.fill();
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.arc(x, y - shift, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "black";
+                ctx.fillStyle = "white";
+                ctx.fill();
+                ctx.stroke();
+                break;
+            case Piece.DAME_BLACK:
+                ctx.beginPath();
+                ctx.arc(x, y + shift, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "white";
+                ctx.fillStyle = "black";
+                ctx.fill();
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.arc(x, y - shift, r ,0, Math.PI * 2, true);
+                ctx.strokeStyle = "white";
+                ctx.fillStyle = "black";
+                ctx.fill();
+                ctx.stroke();
+                break;
+            default:
+            }
+        };
+
+        var drawSquare = function(ctx, num, piece){
+            var nRow = ~~((num - 1) / 5) + 1; // [1..10]
+            var nCol = ((num - 1) % 5) + 1; // [1..5]
+
+            var sqWidth = plugin.options['cvSquareSize'];
+
+            if (nRow == 1 || nRow == 3 || nRow == 5 || nRow == 7 || nRow == 9) {
+                
+                var x = 2 * sqWidth * (nCol - 1) + sqWidth;
+                var y = sqWidth * (nRow - 1);
+
+                ctx.fillStyle = plugin.options['cvSquareDarkColor'];
+                ctx.fillRect(x, y, sqWidth, sqWidth);
+
+                var x = x + (sqWidth / 2);
+                var y = y + (sqWidth / 2);
+                var r = sqWidth / 2 - (sqWidth / 10 * 2);
+
+                drawPiece(ctx, piece, x, y, r);
+            }
+
+            if (nRow == 2 || nRow == 4 || nRow == 6 || nRow == 8 || nRow == 10) {
+                var x = 2 * sqWidth * (nCol - 1);
+                var y = sqWidth * (nRow - 1);
+
+                ctx.fillStyle = plugin.options['cvSquareDarkColor'];
+                ctx.fillRect(x, y, sqWidth, sqWidth);
+
+                var x = x + (sqWidth / 2);
+                var y = y + (sqWidth / 2);
+                var r = sqWidth / 2 - (sqWidth / 10 * 2);
+
+                drawPiece(ctx, piece, x, y, r);
+            }
+        };
+
+        var drawCanvasContent = function(idCanvas, board){
+            var c = document.getElementById(idCanvas);
+            var ctx = c.getContext("2d");
+
+            var sqWidth = plugin.options['cvSquareSize'];
+            c.width = 10*sqWidth;
+            c.height = 10*sqWidth;
+
+            c.style.border = "1px dotted #000";
+            
+            // background-color
+            ctx.fillStyle = plugin.options['cvSquareLightColor'];
+            ctx.fillRect(0, 0, 10*sqWidth, 10*sqWidth);
+
+            for (var num = 1; num <= 50; num++){
+                var p = board.getSquare(num).piece;
+                drawSquare(ctx, num, p);
+            }
+        };
+
 
 
         plugin.init();
-    }
-
-
-    var getNotationStyle = function(notation){
-        var idxSep = notation.indexOf("-");
-        if (idxSep == -1) {
-            idxSep = notation.indexOf("x");
-            notation = notation.replace("x", "&times;");
-        } else {
-            notation = notation.replace("-", "&minus;");
-        }
-        var s = notation.substring(idxSep + 1);
-
-        var before = (idxSep == 1);
-        var after = (s.length == 1);
-
-        var map = { 'notation':notation, 'before':before, 'after':after };
-        return map;
-    }
-
-    var getASCIIBoard = function(board) {
-        var s = "";
-        
-        var cr = "<br />";
-        var sp = "&nbsp;";
-        var row = "";
-
-        s += "================================" + cr;
-        for (var k = 0; k < 10; k++) {
-            row += "|";
-            for (var l = 1; l <= 5; l++) {
-
-                var num = 5 * k + l;
-                var p = board.getSquare(num).piece;
-
-                if (k == 0 || k == 2 || k == 4 || k == 6 || k == 8) {
-                    row += sp + sp + sp;
-                }
-
-                switch (p) {
-                case Piece.PAWN_WHITE:
-                    row += sp + "o" + sp;
-                    break;
-                case Piece.PAWN_BLACK:
-                    row += sp + "x" + sp;
-                    break;
-                case Piece.DAME_WHITE:
-                    row += sp + "O" + sp;
-                    break;
-                case Piece.DAME_BLACK:
-                    row += sp + "X" + sp;
-                    break;
-                default:
-                    row += sp + "." + sp;
-                }
-
-                if (k == 1 || k == 3 || k == 5 || k == 7 || k == 9) {
-                    row += sp + sp + sp;
-                }
-            }
-            row += "|";
-
-            s += row + cr;
-            row = "";
-        }
-        s += "================================";
-
-        return s;
     };
+
+
+    
 
 
 
     $.fn.player = function(options) {
         return this.each(function() {
-
             if (undefined == $(this).data('player')) {
                 var plugin = new $.player(this, options);
                 $(this).data('player', plugin);
             }
         });
-    }
+    };
 
 })(jQuery);
 
