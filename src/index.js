518e076e8e5adf45d8da47b5e36d182f62335dbe
jlfy <julienlfy@gmail.com>
2015-02-17 21:48:46 +0100

    Entry point

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var
+    symbols = require('./utils/symbols'),
+    Game = require('./core/Game'),
+    Piece = symbols.Piece
+;
+
+window.Piece = Piece;
+window.Game = Game;
+
---
+++ src/main.js
@@ -0,0 +1,5 @@
+var
+    symbols = require('./utils/symbols'),
+    Game = require('./core/Game'),
+    Piece = symbols.Piece
+;
