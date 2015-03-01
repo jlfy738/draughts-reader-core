128c21e037cb63306b993cd73dc43431ca6f9aed
jlfy <julienlfy@gmail.com>
2015-03-01 11:53:10 +0100

    Bump release v1.0.0

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-var
-    symbols = require('./utils/symbols'),
-    Game = require('./core/Game'),
-    Piece = symbols.Piece
-;
+exports.Game = require('./core/Game');
+exports.Piece = require('./utils/symbols').Piece;
+
