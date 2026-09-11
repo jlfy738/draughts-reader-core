fcf5a687a7f33c08978603ae795225b2769effb4
julienlfy <julienlfy@gmail.com>
2026-09-11 21:52:15 +0200

    gulp minify

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The draughts-reader-core library is a pure javascript engine to define and read 
 ## Quick start
 - Install : `$ npm install`
 - Launch tests : `$ npm test`
-- Build : `$ npm run build` (NB : using esbuild via gulp ; produces `dist/draughts-reader.core.js`, a browser `<script>`-ready bundle exposing the global `DraughtsReaderCore` object)
+- Build : `$ npm run build` (NB : using esbuild via gulp ; produces `dist/draughts-reader.core.js` and its minified version `dist/draughts-reader.core.min.js`, both browser `<script>`-ready bundles exposing the global `DraughtsReaderCore` object)
 
 
 ## Simple example
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const esbuild = require('esbuild');
 
 const DIST_DIR = './dist';
 const OUTPUT_NAME = 'draughts-reader.core.js';
+const MINIFIED_OUTPUT_NAME = 'draughts-reader.core.min.js';
 
 function clean(done) {
     fs.rm(DIST_DIR, { recursive: true, force: true }, done);
@@ -19,6 +20,18 @@ function build() {
     });
 }
 
+function minify() {
+    return esbuild.build({
+        entryPoints: ['./src/main.js'],
+        outfile: `${DIST_DIR}/${MINIFIED_OUTPUT_NAME}`,
+        bundle: true,
+        format: 'iife',
+        globalName: 'DraughtsReaderCore',
+        minify: true,
+    });
+}
+
 exports.clean = clean;
 exports.build = build;
-exports.default = gulp.series(clean, build);
+exports.minify = minify;
+exports.default = gulp.series(clean, gulp.parallel(build, minify));
